4d3cbca42273a024dee9426472f9b2e061de2d07
aurian.jault <aurian.jault@etu.uca.fr>
2023-03-24 16:50:03 +0100

    approval

---
+++ approval/src/test/java/approval/manager/approvalManager/ApprovalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package approval.manager.approvalManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprovalManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
